01b56799b2c4cd45286a1bd1946fb6d9a43ed6c9
Daniil <dedda2@yandex.ru>
2021-01-15 17:15:24 +0300

    За один цикл

--- src/ru/testForJob/Rhombus.java
+++ src/ru/testForJob/Rhombus.java
@@ -12,46 +12,48 @@ public class Rhombus {
     }
 
     private static void printDiamond(int height, int width) {
-        String[][] field = new String[height][width];
         int x = width / 2;
         int y = height / 2;
         int lastX = width - 1;
         int lastY = height - 1;
-        field[0][width / 2] = "#";
-        field[height - 1][width / 2] = "#";
-        for (int i = 0; i < height; i++) {
-            int oldCheckPoint = 0;
-            for (int j = 0; j < width; j++) {
-                int checkPoint = 0;
-                if (j <= x && i <= y) {
-                    checkPoint = checkVector(0, y, x, 0, j, i);
-                }
-                if (j > x && i <= y) {
-                    checkPoint = checkVector(x, 0, lastX, y, j, i);
-
-                }
-                if (j <= x && i > y) {
-                    checkPoint = checkVector(0, y, x, lastY, j, i);
-
-                }
-                if (j > x && i > y) {
-                    checkPoint = checkVector(x, lastY, lastX, y, j, i);
-
-                }
-                if (checkPoint == 0) {
-                    field[i][j] = "#";
-                } else if (checkPoint * oldCheckPoint < 0) {
-                    field[i][j] = "#";
-
-                } else {
-                    field[i][j] = " ";
-                }
-                oldCheckPoint = checkPoint;
-                System.out.print(field[i][j]);
-
+        int oldCheckPoint = 0;
+        int checkPoint = 0;
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < height * width; k++) {
+            if (j <= x && i <= y) {
+                checkPoint = checkVector(0, y, x, 0, j, i);
             }
-            System.out.println();
+            if (j > x && i <= y) {
+                checkPoint = checkVector(x, 0, lastX, y, j, i);
+            }
+            if (j <= x && i > y) {
+                checkPoint = checkVector(0, y, x, lastY, j, i);
+            }
+            if (j > x && i > y) {
+                checkPoint = checkVector(x, lastY, lastX, y, j, i);
+            }
+
+            if (checkPoint == 0) {
+                System.out.print("#");
+            } else if (checkPoint * oldCheckPoint < 0) {
+                System.out.print("#");
+
+            } else {
+                System.out.print(" ");
+            }
+            oldCheckPoint = checkPoint;
+
+            if (j < width - 1) {
+                j++;
+            } else {
+                j = 0;
+                i++;
+                System.out.println();
+                oldCheckPoint = 0;
+            }
         }
+
     }
 
     private static int checkVector(int x1, int y1, int x2, int y2, int x, int y) {
